730280166635f280bb5ea7fc09f807adae2cdff7
jhon <jhonatangs1@gmail.com>
2020-12-21 00:26:39 -0300

    test dir actions

--- actions/test_performance_actions.py
+++ actions/test_performance_actions.py
@@ -5,45 +5,8 @@ from sklearn.tree import DecisionTreeClassifier
 import numpy as np
 import pandas as pd
 
-from slsdt.reader_csv import read_csv
-from slsdt.slsdt import SLSDT
+import os
 
-
-clfs = [DecisionTreeClassifier(), SLSDT()]
-instances = glob.glob("instances_actions/*.csv")
-results = []
-
-for instance in instances:
-    print("Testando: ", instance)
-
-    result = []
-    result.append(instance)
-
-    X, y = read_csv(instance, "class")
-
-    result.append(X.shape[0])
-    result.append(X.shape[1])
-    result.append(np.unique(y).shape[0])
-
-    for clf in clfs:
-        cv = StratifiedKFold(n_splits=5)
-        result.append(
-            cross_val_score(clf, X, y, cv=cv, scoring="accuracy", n_jobs=-1).mean()
-        )
-
-    results.append(result)
-
-df = pd.DataFrame(
-    results,
-    columns=[
-        "Instance",
-        "Number Of Objects",
-        "Number Of Features",
-        "Number Of Classes",
-        "DecisionTreeClassifier",
-        "SLSDT",
-    ],
-)
-df = df.round(3)
-print(df)
-df.to_csv("results/result_performance_slsdt.csv")
+print(os.getcwd())
+print(glob.glob("./*.py"))
+print(glob.glob("./*.csv"))
